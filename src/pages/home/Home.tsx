84edc95a83b596c814a02cc9cbca1935bd17f43f
enriquevillon <enriquevillon2597@gmail.com>
2024-08-04 11:47:27 -0500

    first commit

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Home.scss";
+import { Header } from "../../components/header/Header";
+export const Home = () => {
+  return (
+    <div className="container">
+      <Header />
+      Home
+    </div>
+  );
+};
---
+++ src/styles/Theme.tsx
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FF5733",
+    },
+    secondary: {
+      main: "#f4d03f",
+    },
+  },
+});
