6c08679682998be9928556ff52f55c94f2f48617
enriquevillon <enriquevillon2597@gmail.com>
2024-08-05 23:11:32 -0500

    feature/create-detail

--- src/pages/detail/Detail.tsx
+++ src/pages/detail/Detail.tsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { TextField } from "@mui/material";
 export const Detail = () => {
+  const min = 1;
+  const max = 2;
+  const handleChange = (event: any) => {
+    const value = parseInt(event.target.value, 10);
+    if (!isNaN(value) && value >= min && value <= max) {
+      setQuantity(value);
+    }
+  };
+  const [quantity, setQuantity] = useState(min);
+
   return (
     <div>
       <div>
-        <KeyboardArrowUpIcon />
-        <KeyboardArrowDownIcon />
+        <span> Cantidad</span>
+        <TextField
+          type="number"
+          value={quantity}
+          onChange={handleChange}
+          inputProps={{ min, max }}
+          size="small"
+          variant="outlined"
+          style={{ width: "60px", margin: "0 10px", background: "white" }}
+        />
+        {/* <div style={{ display: "flex", flexDirection: "column" }}>
+          <KeyboardArrowUpIcon />
+          <KeyboardArrowDownIcon />
+        </div> */}
       </div>
     </div>
   );
