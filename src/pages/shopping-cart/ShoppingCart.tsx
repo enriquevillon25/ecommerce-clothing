1d459f320b7488b48b1186dc0c109b2ac10a66ca
enriquevillon <enriquevillon2597@gmail.com>
2024-08-07 21:51:00 -0500

    feature/install-mercad-pago

--- src/pages/shopping-cart/ShoppingCart.tsx
+++ src/pages/shopping-cart/ShoppingCart.tsx
@@ -1,5 +1,19 @@
 import React from "react";
 
+import { initMercadoPago, Wallet } from "@mercadopago/sdk-react";
+
 export const ShoppingCart = () => {
-  return <div>ShoppingCart</div>;
+  initMercadoPago("YOUR_PUBLIC_KEY", { locale: "es-PE" });
+  return (
+    <div>
+      ShoppingCart
+      <div>
+        Comprar
+        <Wallet
+          initialization={{ preferenceId: "<PREFERENCE_ID>" }}
+          customization={{ texts: { valueProp: "smart_option" } }}
+        />
+      </div>
+    </div>
+  );
 };
